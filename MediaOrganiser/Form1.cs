dc4d2d00ef63419f0f84d70fb9e4feb0ce72874d
rjamieson <rach_16390@hotmail.com>
2016-05-07 14:51:37 +0100

    2nd branch save

--- MediaOrganiser/Form1.cs
+++ MediaOrganiser/Form1.cs
@@ -37,6 +37,7 @@ namespace MediaOrganiser
         {
             var subItems = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
             Drive drive = new Drive { Name = path };
+            var y = "another random string";
             foreach(var item in subItems)
             {
                 if (File.Exists(item))
